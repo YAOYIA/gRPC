b919c6c71b9234cc882876d2e954ba80894363c7
wangkaiyi <kaiyiwang7@163.com>
2023-04-12 18:50:19 +0800

    add 6 files

---
+++ greeter_client.cc
@@ -0,0 +1,108 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/helloworld.grpc.pb.h"
+#else
+#include "helloworld.grpc.pb.h"
+#endif
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::MyMessage;
+using helloworld::MyMessageService;
+
+class MyMessageClient {
+ public:
+  MyMessageClient(std::shared_ptr<Channel> channel)
+      : stub_(MyMessageService::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  //std::string SendMessage(const std::string& message) {
+    MyMessage SendMessage(const MyMessage& message) {
+    // Data we are sending to the server.
+
+    // Container for the data we expect from the server.
+    MyMessage response;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->SendMessage(&context, message, &response);
+
+    // Act upon its status.
+    if (status.ok()) {
+      std::cout << "Message sent successfully." << std::endl;
+    } else {
+      std::cout << "Failed to send message. Error code: " << status.error_code()
+                << ", Error message: " << status.error_message() << std::endl;
+    }
+    
+    return response;
+  }
+
+ private:
+  std::unique_ptr<MyMessageService::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint specified by
+  // the argument "--target=" which is the only expected argument.
+  // We indicate that the channel isn't authenticated (use of
+  // InsecureChannelCredentials()).
+  std::string target_str;
+  std::string arg_str("--target");
+  if (argc > 1) {
+    std::string arg_val = argv[1];
+    size_t start_pos = arg_val.find(arg_str);
+    if (start_pos != std::string::npos) {
+      start_pos += arg_str.size();
+      if (arg_val[start_pos] == '=') {
+        target_str = arg_val.substr(start_pos + 1);
+      } else {
+        std::cout << "The only correct argument syntax is --target="
+                  << std::endl;
+        return 0;
+      }
+    } else {
+      std::cout << "The only acceptable argument is --target=" << std::endl;
+      return 0;
+    }
+  } else {
+    target_str = "localhost:50051";
+  }
+  MyMessage my_message;
+  my_message.set_name("LiFeng");
+  my_message.set_age(23);
+  MyMessageClient client(
+      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+  client.SendMessage(my_message);
+
+  return 0;
+}
